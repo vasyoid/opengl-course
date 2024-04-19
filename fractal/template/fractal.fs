0600d8e0f8362c958f1e08f6402de4d2809a3b0b
vasyoid <vasyoid@gmail.com>
2024-04-19 22:07:59 +0200

    Lesson 4 template update

--- fractal/template/fractal.fs
+++ fractal/template/fractal.fs
@@ -4,6 +4,12 @@ in vec3 pos;
 
 out vec4 fragColor;
 
+vec2 f(vec2 z, vec2 c)
+{
+    return vec2(z.x * z.x - z.y * z.y, 2 * z.x * z.y) + c;
+}
+
+
 void main()
 {
     fragColor = vec4(1, 1, 1, 1);
